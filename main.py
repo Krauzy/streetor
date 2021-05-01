88a8dcdab664de09593a314d046d9baf12470772
Krauzy <30305307+Krauzy@users.noreply.github.com>
2021-05-01 00:03:53 -0300

    KMeans added

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+from sklearn.cluster import KMeans
 
 
 def plot_map(mapbox_style='carto-positron', period='Dawn', src_color='black', pred_color='red'):
+    kmeans = KMeans(40)
     sp = pd.read_excel('./resources/dataset.xlsx')
     sp = sp[sp['Period'] == period]
+    #print(kmeans)
+    sp['cluster'] = kmeans.fit_predict(sp[['Latitude','Longitude']])
     fig = px.scatter_mapbox(sp,
                             lat='Latitude',
                             lon='Longitude',
@@ -14,7 +18,7 @@ def plot_map(mapbox_style='carto-positron', period='Dawn', src_color='black', pr
                             color_discrete_sequence=[src_color, pred_color],
                             zoom=9,
                             height=500,
-                            color='Sex')
+                            color='cluster')
     fig.update_layout(mapbox_style=mapbox_style)
     fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
     fig.update_traces(mode='markers', marker=dict(size=8))
@@ -41,8 +45,7 @@ def init():
     else:
         period = 'Dawn'
     st.sidebar.title('Settings')
-    op = st.sidebar.selectbox(label='Theme',
-                              options=['Default', 'Dark', 'Open Street'])
+    op = st.sidebar.selectbox(label='Tema', options=['Open Street', 'Light', 'Dark'])
     side1, side2 = st.sidebar.beta_columns([1, 1])
     c = side1.color_picker(label='Pontos Originais:', value='#000000')
     color_pred = side2.color_picker(label='Pontos Preditivos:', value='#F15656')
@@ -56,5 +59,5 @@ def init():
     else:
         plot_map(mapbox_style='open-street-map', period=period, src_color=c, pred_color=color_pred)
 
-
+        
 init()
